c66d5c311431850ffbf8d7118c771c93a89d1343
suryabinamare <97321080+suryabinamare@users.noreply.github.com>
2025-07-24 15:20:13 -0500

    Update linear.py

--- linear.py
+++ linear.py
@@ -28,7 +28,81 @@ st.markdown("""First we will plot the data points in a scatter plot, and then we
 
 st.markdown("### Scatter Plot:")
 
-# File uploader
+# Function to create a scatter plot
+@st.cache_data
+def scatter_plot(df, x, y):
+    # Plot the scatter plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(df[x], df[y], marker='o', color='red')
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
+    ax.set_title(f"Scatter Plot of {x} vs {y}")
+    ax.grid(True)
+    st.pyplot(fig)
+    return fig
+
+#function to calculate various values for linear regression
+@st.cache_data
+def calculate_regression_values(df, x_axis, y_axis):
+    n, m = df.shape
+    X_bar = round(df[x_axis].mean(), 3)
+    Y_bar = round(df[y_axis].mean(), 3)
+    S_xx = ((df[x_axis] - X_bar) ** 2).sum()
+    S_yy = ((df[y_axis] - Y_bar) ** 2).sum()
+    S_xy = ((df[x_axis] - X_bar) * (df[y_axis] - Y_bar)).sum()
+    slope = round(S_xy / S_xx, 3)
+    b = round(1/n * (df[y_axis].sum() - slope * df[x_axis].sum()),3)
+    R_squared = round(S_xy ** 2 / (S_xx * S_yy), 3)
+    r = round(R_squared**0.5, 3)
+
+    return slope, b, R_squared, r, X_bar, Y_bar, S_xx, S_yy, S_xy
+
+#function to display regression line equation
+@st.cache_data
+def display_regression_equation(slope, intercept):
+    x, y = sp.symbols('x y')
+    
+    # Create the equation: y = slope * x + intercept
+    eqn = sp.Eq(y, slope * x + intercept)
+    
+    # Display the equation in LaTeX using Streamlit
+    return sp.latex(eqn)
+
+
+#function to display regression line:
+@st.cache_data
+def display_regression_line(df, x_axis, y_axis, slope, intercept):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(df[x_axis], df[y_axis], marker='o', color='red', label='Data Points')
+    ax.plot(df[x_axis], slope * df[x_axis] + intercept, color='blue', label='Regression Line')
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+    ax.set_title(f"Scatter Plot with Regression Line of {y_axis} vs {x_axis}")
+    plt.grid(True)
+    plt.legend()
+    st.pyplot(fig)
+    return fig
+
+
+#predicting values using the regression line
+@st.cache_data
+def predict_value(slope, intercept, x_value):
+    """
+    Predicts the value of Y for a given X using the regression line equation.
+    
+    Parameters:
+    slope (float): The slope of the regression line.
+    intercept (float): The y-intercept of the regression line.
+    x_value (float): The value of X for which to predict Y.
+    
+    Returns:
+    float: The predicted value of Y.
+    """
+    return slope * x_value + intercept
+
+
+
+# File uploader for CSV file
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 # Check if a file has been uploaded
@@ -47,66 +121,55 @@ if uploaded_file is not None:
         x_axis = st.selectbox("Select X-axis column", numeric_columns)        
         y_axis = st.selectbox("Select Y-axis column", numeric_columns, index=1)
 
-        # Plot the scatter plot
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.scatter(df[x_axis], df[y_axis], marker ='o', color='red')
-        ax.set_xlabel(x_axis)
-        ax.set_ylabel(y_axis)
-        ax.set_title(f"Scatter Plot of {y_axis} vs {x_axis}")
-        plt.grid(True)
-        st.pyplot(fig)
+        figure = scatter_plot(df, x_axis, y_axis)
+        st.markdown(figure)
 
 
         st.markdown("""Does the data align along a straight line? Lets find line that best fits the data""")
         st.markdown('The best fit line has an equation of the form: $\\bar{y} = mx + b$, where $m$ is the slope and $b$ is the y-intercept.')
         st.markdown('We calculate the slope and y-intercept using the following formulas by minimizing the sum of squares of the residuals:')
 
-        n, m = df.shape
-        X_bar = round(df[x_axis].mean(), 3)
-        Y_bar = round(df[y_axis].mean(), 3)
-        S_xx = ((df[x_axis] - X_bar) ** 2).sum()
-        S_yy = ((df[y_axis] - Y_bar) ** 2).sum()
-        S_xy = ((df[x_axis] - X_bar) * (df[y_axis] - Y_bar)).sum()
-        slope = round(S_xy / S_xx, 3)
-        b = round(1/n * (df[y_axis].sum() - slope * df[x_axis].sum()),3)
-        R_squared = round(S_xy ** 2 / (S_xx * S_yy), 3)
 
-        st.markdown(f"""
-        - ###### Mean of X = $$\\bar{{X}} = $$ {X_bar}
-        - ###### Mean of Y = $$\\bar{{Y}} = $$ {Y_bar}
-        - ###### Sum of Squares of X = $$S_{{xx}} = \\sum (x_i-\\bar{{X}})^2 = $$ {round(S_xx,3)}
-        - ###### Sum of Squares of Y = $$S_{{yy}} = \\sum (y_i-\\bar{{Y}})^2 = $$ {round(S_yy,3)}
-        - ###### Sum of Cross Products = $$S_{{xy}} = \\sum (x_i-\\bar{{X}})(y_i-\\bar{{Y}}) = $$ {round(S_xy,3)}
-        - ###### Slope of the line: $$m = $$  {slope}
-        - ###### Y-intercept of the line: $$b = $$ {b}
-        - ###### Coefficient of Determination $R^2 = $ {R_squared}
-        - ###### Linear Correlation Coefficient $r = \\sqrt{{R^2}} = $ {round(R_squared**0.5, 3)}
+        #calling the function to calculate regression values
+        slope, b, R_squared, r, X_bar, Y_bar, S_xx, S_yy, S_xy = calculate_regression_values(df, x_axis, y_axis)
+
+        st.markdown("#### Regression Analysis Results:")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown(f"""
+            - ###### Mean of X = $$\\bar{{X}} = $$ {X_bar}
+            - ###### Mean of Y = $$\\bar{{Y}} = $$ {Y_bar}
+            - ###### $$S_{{xx}} = \\sum (x_i-\\bar{{X}})^2 = $$ {round(S_xx,3)}
+            - ###### $$S_{{yy}} = \\sum (y_i-\\bar{{Y}})^2 = $$ {round(S_yy,3)}
+            - ###### $$S_{{xy}} = \\sum (x_i-\\bar{{X}})(y_i-\\bar{{Y}}) = $$ {round(S_xy,3)}
+                    """)
+        with col2:
+            st.markdown(f"""
+            - ###### Slope: $$\quad m = $$  {slope}
+            - ###### Y-intercept: $$\quad b = $$ {b}
+            - ###### Coef of Determination: $\quad R^2 = $ {R_squared}
+            - ###### Linear Correlation Coef: $\quad r = \\sqrt{{R^2}} = $ {r}
                     """)
     
 
-        x, y = sp.symbols('x y')
-        eqn = slope * x + b
-        equn = sp.Eq(y, eqn)
         
-        st.markdown(f""" ##### The equation of the line is: {sp.latex(equn)}""")
-
+        #display the regression equation
+        eqn = display_regression_equation(slope, b)
+        st.markdown(f""" ##### The equation of the line is:$\quad$ {eqn}""")
+        st.markdown('#### Regression Line Plot:')
         if st.button("##### Plot Regression Line"):
-            fig, ax = plt.subplots(figsize=(10, 6))
-            ax.scatter(df[x_axis], df[y_axis], marker='o', color='red', label='Data Points')
-            ax.plot(df[x_axis], slope * df[x_axis] + b, color='blue', label='Regression Line')
-            ax.set_xlabel(x_axis)
-            ax.set_ylabel(y_axis)
-            ax.set_title(f"Scatter Plot with Regression Line of {y_axis} vs {x_axis}")
-            plt.grid(True)
-            plt.legend()
-            st.pyplot(fig)
+            figure = display_regression_line(df, x_axis, y_axis, slope, b)
+            st.markdown(figure)
+
+
 
         st.markdown('#### Predicting Values:')
         st.write("You can use the equation of the line to predict values of Y for given values of X.")
         X_new = st.number_input("Enter a value for X:", value=0.0)
         if st.button("Predict"):
-            Y_pred = m * X_new + b
-            st.write(f"The predicted value of Y for X = {X_new} is: {Y_pred}") 
+            Y_pred = predict_value(slope, b, X_new)
+            st.markdown(f"##### The predicted value of Y for X = {X_new} is $\quad $ {Y_pred}")
+        
 
 
 else:
